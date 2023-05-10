27da91b96a73a45626b6c0437d8a03366e9e264e
Raphael-Boichot <63796796+Raphael-Boichot@users.noreply.github.com>
2023-05-10 23:06:25 +0200

    Delete send_packet.m

--- Research/send_packet.m
+++
@@ -1,6 +0,0 @@
-function [packet_RX]=send_packet(packet_TX,arduinoObj)
-packet_RX=zeros(length(packet_TX));
-for i=1:1:length(packet_TX)
-    fwrite(arduinoObj,packet_TX(i))
-    packet_RX(i) = fread(arduinoObj,1);
-end
